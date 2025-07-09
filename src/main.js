8d0dfc38cd48a337650492f31b7513a5b41f5498
anlin <397620663@qq.com>
2025-07-09 16:29:27 +0800

    chore: 移除冲突依赖，成功安装 element-plus

--- src/main.js
+++ src/main.js
@@ -6,11 +6,15 @@ import router from './router'
 import '@/styles/reset.css';
 import '@/styles/variables.css';
 import '@/styles/global.css';
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 app.use(createPinia())  // 注册Pinia
 app.mount('#app')
 app.use(router) 
+app.use(ElementPlus)
+
 
 if (import.meta.env.VITE_MOCK_ENABLE) {
   import('./mock').then(() => console.log('Mock 启用'));
